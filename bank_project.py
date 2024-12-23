51490ab0b60b0ca168e9acdc305371fc6cd3ab24
Quentin Cartier <daoliangshumaishuji@gmail.com>
2024-12-23 21:45:38 +0100

    Initial commit

---
+++ bank_project.py
@@ -0,0 +1,74 @@
+# Code for ETL operations on Country-GDP data
+
+# Importing the required libraries
+from bs4 import BeautifulSoup
+import requests
+import pandas as pd
+import numpy as np
+import sqlite3
+from datetime import datetime 
+
+url = 'https://web.archive.org/web/20230908091635/https://en.wikipedia.org/wiki/List_of_largest_banks'
+table_attribs = ["Rank", "Bank name", "Market Cap (US$ billion)"]
+
+
+def log_progress(message):
+    ''' This function logs the mentioned message of a given stage of the
+    code execution to a log file. Function returns nothing'''
+    timestamp_format = '%Y-%h-%d-%H:%M:%S'
+    now = datetime.now() # get current timestamp 
+    timestamp = now.strftime(timestamp_format) 
+    with open("./code_log.txt","a") as f: 
+        f.write(timestamp + ' : ' + message + '\n')
+
+
+def extract(url, table_attribs):
+    ''' This function aims to extract the required
+    information from the website and save it to a data frame. The
+    function returns the data frame for further processing. '''
+    page = requests.get(url).text
+    soup = BeautifulSoup(page, 'html.parser')
+    df = pd.DataFrame(columns=table_attribs)
+    tables = soup.find_all('tbody')
+    rows = tables[0].find_all('tr')
+    for row in rows:
+        col = row.find_all('td')
+        if len(col)!=0:
+            if col[1].findAll('a')[1] is not None and '—' not in col[2]:
+                data_dict = {"Rank": col[0].contents[0].rstrip(),
+                             "Bank name": col[1].findAll('a')[1].contents[0].rstrip(),
+                             "Market Cap (US$ billion)": col[2].contents[0].rstrip()}
+                df1 = pd.DataFrame(data_dict, index=[0])
+                df = pd.concat([df,df1], ignore_index=True)
+    return df
+
+def transform(df, csv_path):
+    ''' This function accesses the CSV file for exchange rate
+    information, and adds three columns to the data frame, each
+    containing the transformed version of Market Cap column to
+    respective currencies'''
+
+    return df
+
+def load_to_csv(df, output_path):
+    ''' This function saves the final data frame as a CSV file in
+    the provided path. Function returns nothing.'''
+
+def load_to_db(df, sql_connection, table_name):
+    ''' This function saves the final data frame to a database
+    table with the provided name. Function returns nothing.'''
+
+def run_query(query_statement, sql_connection):
+    ''' This function runs the query on the database table and
+    prints the output on the terminal. Function returns nothing. '''
+
+''' Here, you define the required entities and call the relevant
+functions in the correct order to complete the project. Note that this
+portion is not inside any function.'''
+
+log_progress('Preliminaries complete. Initiating ETL process')
+df = extract(url, table_attribs)
+log_progress('Data extraction complete. Initiating Transformation process')
+
+print(df.shape)
+print(df)
